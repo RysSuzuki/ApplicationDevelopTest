e0eaf3365a40f2af0225d90d8696319da8e94553
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-11-30 14:40:49 +0900

    add:アジェンダ削除機能追加

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,6 @@
 class AgendasController < ApplicationController
   # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: [:destroy]
 
   def index
     @agendas = Agenda.all
@@ -11,14 +12,22 @@ class AgendasController < ApplicationController
   end
 
   def create
-    @agenda = current_user.agendas.build(agenda_params)
+    @agenda = current_user.agendas.build(title: params[:title])
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
-    binding.pry
     if current_user.save && @agenda.save
       redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
     else
       render :new
+    end
+  end
+
+  def destroy
+    if @agenda.user_id == current_user.id || @agenda.team.owner_id == current_user.id
+      @agenda.destroy
+      redirect_to dashboard_url, notice: I18n.t('views.messages.destroy_agenda') 
+    else
+      redirect_to dashboard_url, notice: I18n.t('views.messages.not_destroy_agenda') 
     end
   end
 
